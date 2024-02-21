f7fb114e719745cf3c905bc953d82875a6789b06
RobinInagan <rsinagano@correo.udistrital.edu.co>
2024-02-21 18:55:22 -0500

    Ejercicio Terminado

--- main.py
+++ main.py
@@ -15,5 +15,29 @@ except:
 if option not in [1,2,3]:
     print ("Valor no valido")
     exit()
+    
+if(1 == option):
+    try:
+        a=int(input(f'\n\n_Ingrese los valores a Sumar \n----> a = '))
+        b=int(input('\n----> b = '))
 
+        print(f'\nLa suma de {a}+{b} es igual a: {a+b}')
+    except ValueError:
+            input('Error, valor ingresado o valido \nPresione ENTER para continuar ...')
+if(2 == option):
+    try:
+        a=int(input(f'\n\n_Ingrese los valores a Restar \n----> a = '))
+        b=int(input('\n----> b = '))
 
+        print(f'\nLa resta de {a}-{b} es igual a: {a-b}')
+    except ValueError:
+            input('Error, valor ingresado o valido \nPresione ENTER para continuar ...')     
+if(3 == option):
+    try:
+        a=int(input(f'\n\n_Ingrese los valores a Dividir \n----> a = '))
+        b=int(input('\n----> b = '))
+        print(f'\nLa división de {a}/{b} es igual a: {a/b}')
+    except ValueError:
+            input('Error, valor ingresado o valido \nPresione ENTER para continuar ...')
+    except ZeroDivisionError:
+            input('Error, no existe la división por 0 \nPresione ENTER para continuar ...')
